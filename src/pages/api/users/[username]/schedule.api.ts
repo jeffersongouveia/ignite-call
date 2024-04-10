edaa620d54a16086ab230672eb23889f8c82cb96
Jeff Gouveia <jeff.gouveia@hotmail.com>
2024-04-10 12:17:45 -0400

    📅 Saving schedule on Google Calendar

--- src/pages/api/users/[username]/schedule.api.ts
+++ src/pages/api/users/[username]/schedule.api.ts
@@ -1,8 +1,10 @@
 import { NextApiRequest, NextApiResponse } from 'next'
+import { google } from 'googleapis'
 import dayjs from 'dayjs'
 import z from 'zod'
 
 import { prisma } from '../../../../lib/prisma'
+import { getGoogleOAuthToken } from '../../../../lib/google'
 
 export default async function handler(
   req: NextApiRequest,
@@ -55,13 +57,47 @@ export default async function handler(
     })
   }
 
-  await prisma.scheduling.create({
+  const scheduling = await prisma.scheduling.create({
     data: {
       name,
       email,
       observations,
       date: schedulingDate.toDate(),
       user_id: user.id,
+    },
+  })
+
+  const calendar = google.calendar({
+    version: 'v3',
+    auth: await getGoogleOAuthToken(user.id),
+  })
+
+  await calendar.events.insert({
+    calendarId: 'primary',
+    conferenceDataVersion: 1,
+    requestBody: {
+      summary: `Ignite Call: ${name}`,
+      description: observations,
+      start: {
+        dateTime: schedulingDate.format(),
+      },
+      end: {
+        dateTime: schedulingDate.add(1, 'hour').format(),
+      },
+      attendees: [
+        {
+          displayName: name,
+          email,
+        },
+      ],
+      conferenceData: {
+        createRequest: {
+          requestId: scheduling.id,
+          conferenceSolutionKey: {
+            type: 'hangoutsMeet',
+          },
+        },
+      },
     },
   })
 
